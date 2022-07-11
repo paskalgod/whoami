da7030b2886e2322a9cd6ee9e980c1222dc004ae
Daniil <Daniil@DESKTOP-BNBNJDL>
2022-07-12 00:22:08 +0300

    Add testing files

--- WhoamiApplication/Program.cs
+++ WhoamiApplication/Program.cs
@@ -8,10 +8,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace WhoamiApplication
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
